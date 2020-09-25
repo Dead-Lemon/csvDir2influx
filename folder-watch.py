53296e86e519c248be2b9e12a845076527f789d0
Dead-Lemon <wslatem@gmail.com>
2020-09-25 18:28:29 +0200

    inotify testing

--- folder-watch.py
+++ folder-watch.py
@@ -1,18 +1,20 @@
 import inotify.adapters
 import argparse
-import csv-to-influxdb
+#import csv-to-influxdb
 
 
 
 def folderwatch(folderpath):
-    i = inotify.adapters.InotifyTree(folderpath)
 
-    for event in i.event_gen():
-        print(event)
+    i = inotify.adapters.Inotify()
 
+    i.add_watch(folderpath)
 
-
-
+    for event in  i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+        print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(path, filename, type_names))
+        if 'IN_CLOSE_WRITE' in type_names:
+            print('found file to have updated')
 
 
 
@@ -25,3 +27,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     folderwatch(args.input)
+
+
+
+#(_INOTIFY_EVENT(wd=1, mask=8, cookie=0, len=32), ['IN_CLOSE_WRITE'], '/home/pointftp/SIM1', u'SIM1_A_20200913.csv')
