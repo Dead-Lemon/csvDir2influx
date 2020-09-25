c21d3283e15c86c3bdc97151bf9b002a6ef6040b
Dead-Lemon <wslatem@gmail.com>
2020-09-25 17:45:35 +0200

    inotify testing

--- folder-watch.py
+++ folder-watch.py
@@ -1,6 +1,17 @@
 import inotify.adapters
+#
+#i = inotify.adapters.InotifyTree('/home/pointftp/SIM1')
+#
+#for event in i.event_gen():
+#    print(event)
 
-i = inotify.adapters.InotifyTree('/home/pointftp/SIM1')
 
-for event in i.event_gen():
-    print(event)
+i = inotify.adapters.inotify()
+
+i.add_watch('/home/pointftp/SIM1')
+
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+
+        print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+              path, filename, type_names))
